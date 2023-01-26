ae8f0e465c9e73f07761619a57bdf4ed2bd9e5c8
Paulo Caetano <paulocaetanomt88@gmail.com>
2023-01-26 18:39:47 -0400

    Definindo títulos na exportação

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,7 +4,9 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
-class TarefasExport implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -15,6 +17,11 @@ class TarefasExport implements FromCollection
          // return auth()->user()->tarefas()->get();
         
          // Forma usando ORM do Laravel Model::where('campo_da_tabela', valor) 
-        return Tarefa::where('user_id', auth()->user()->id)->get();
+        return Tarefa::where('user_id', auth()->user()->id)->get(['id','user_id','tarefa','data_final','created_at','updated_at']);
+    }
+
+    public function headings(): array // declarando o tipo de retorno (tipo array)
+    {
+        return ['ID da Tarefa','ID do Usuário','Tarefa', 'Data final para conclusão','Criada em','Atualizada em'];
     }
 }
